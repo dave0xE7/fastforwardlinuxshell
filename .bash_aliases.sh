c0148eed9a2a4cbd525e644574b325fc96575c27
dave0xE7 <33557528+dave0xE7@users.noreply.github.com>
2020-08-15 09:44:36 +0000

    Create .bash_aliases.sh

---
+++ .bash_aliases.sh
@@ -0,0 +1,17 @@
+
+# more bash aliases
+# nano ~/.bash_aliases
+
+alias s='sudo su'
+alias r='sudo su'
+alias e='exit'
+
+alias apt='sudo apt'
+alias apt-get='sudo apt-get'
+
+
+alias nanos='sudo nano'
+
+alias systemctl='sudo systemctl'
+alias killall='sudo killall'
+alias kill='sudo kill'
