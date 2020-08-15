26b30a586f1bc40c9d99259287caa65e1918258c
dave0xE7 <33557528+dave0xE7@users.noreply.github.com>
2020-08-15 09:46:25 +0000

    Update and rename .bash_aliases.sh to sudoer_bash_aliases.sh

--- sudoer_bash_aliases.sh
+++ sudoer_bash_aliases.sh
@@ -1,16 +1,7 @@
 
-# more bash aliases
-# nano ~/.bash_aliases
-
-alias s='sudo su'
-alias r='sudo su'
-alias e='exit'
 
 alias apt='sudo apt'
 alias apt-get='sudo apt-get'
-
-
-alias nanos='sudo nano'
 
 alias systemctl='sudo systemctl'
 alias killall='sudo killall'
