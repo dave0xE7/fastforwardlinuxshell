0e103c2059873d08126f81bc288a75deb9f64d7c
dave0xE7 <33557528+dave0xE7@users.noreply.github.com>
2020-08-22 01:21:12 +0000

    Update base_bash_aliases.sh

--- base_bash_aliases.sh
+++ base_bash_aliases.sh
@@ -12,3 +12,10 @@ alias nanos='sudo nano'
 alias chm='chmod -R 777'
 
 alias chow='chown -R www-data.www-data'
+
+alias a2restart='systemctl restart apache2'
+alias a2reload='systemctl reload apache2'
+
+alias sshdrestart='systemctl restart sshd'
+
+alias a2errorlog='tail -f /var/log/apache2/error.log'
