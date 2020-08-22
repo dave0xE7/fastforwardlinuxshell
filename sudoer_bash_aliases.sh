e158cd1935d7a1bf0c54e1fa9a17daabbb592d41
dave0xE7 <33557528+dave0xE7@users.noreply.github.com>
2020-08-22 01:12:22 +0000

    Update sudoer_bash_aliases.sh

--- sudoer_bash_aliases.sh
+++ sudoer_bash_aliases.sh
@@ -4,5 +4,10 @@ alias apt='sudo apt'
 alias apt-get='sudo apt-get'
 
 alias systemctl='sudo systemctl'
+alias journalctl='sudo journalctl'
+
 alias killall='sudo killall'
 alias kill='sudo kill'
+
+alias mount='sudo mount'
+alias umount='sudo umount'
